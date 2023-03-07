27404ff2273eec97cddb4559db72ac0275114717
MokshaShah <moksha@simformsolutions.com>
2023-03-07 15:48:50 +0530

    move todo in component

--- app.vue
+++ app.vue
@@ -1,46 +1,9 @@
 <script>
-import { defineNuxtComponent } from "#app";
-export default defineNuxtComponent({
-  data: () => ({
-    todoList: [],
-  }),
-  methods: {
-    fetchTodoList() {
-      fetch("https://jsonplaceholder.typicode.com/todos/")
-        .then((res) => res.json())
-        .then((json) => {
-          this.todoList = json;
-        });
-    },
-  },
-});
+import Todolist from "./components/Todolist";
 </script>
 
 <template>
   <div>
-    <img src="/todo.jpg" alt="Todo Photo" width="600px" />
-    <p>
-      Photo by
-      <a
-        href="https://unsplash.com/@glenncarstenspeters?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
-      >
-        Glenn Carstens-Peters
-      </a>
-      on
-      <a
-        href="https://unsplash.com/photos/RLw-UC03Gwc?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
-        >Unsplash</a
-      >
-    </p>
-    <h1>Hello world!</h1>
-    <button @click="fetchTodoList">Fetch data</button>
-    <!-- <pre>{{ todoList }}</pre> -->
-
-    <ul>
-      <li v-for="todo in todoList" :key="`todo-id-${todo.id}`">
-        <input type="checkbox" :checked="todo.completed" />
-        {{ todo.title }}
-      </li>
-    </ul>
+    <Todolist />
   </div>
 </template>
